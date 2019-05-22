8272062f41417c52081afecde60c43b838ee89c8
03sai <35158530+03sai@users.noreply.github.com>
2019-05-22 20:40:24 +0530

    Update 2.largest of 3 numbers.py

--- 2.largest of 3 numbers.py
+++ 2.largest of 3 numbers.py
@@ -1,9 +1,9 @@
 a=int(input("Enter the values of a="))
 b=int(input("Enter the values of b="))
 c=int(input("Enter the values of c="))
-if(a>b):
+if(a>b) and (a>c):
     print(a,"is largest")
-elif(b>c):
+elif(b>c) and (b>c):
     print(b,"is largest")
 else:
     print(c,"is largest")
